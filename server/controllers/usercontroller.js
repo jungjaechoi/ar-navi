09b91d0d8f747175ed8d33009f37ed23defac74b
jungjaechoi <www898975@gmail.com>
2021-11-08 17:59:44 +0900

    카카오 로그인 반정도 구현

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,3 +1,15 @@
 export const home = async (req, res) => {
     return res.render("index.html");
 }; 
+
+export const down = async(req,res) => {
+    return res.render("down.html");
+};
+
+export const summary = async(req,res) => {
+    return res.render("summary.html");
+};
+
+export const manual = async(req,res) => {
+    return res.render("manual.html");
+};
--- server/routers/rootRouter.js
+++ server/routers/rootRouter.js
@@ -1,8 +1,13 @@
 import express from "express";
-import {home} from "../controllers/usercontroller.js";
+import {home, down, summary, manual} from "../controllers/usercontroller.js";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
+rootRouter.get("/index.html", home);
+rootRouter.get("/down.html", down);
+rootRouter.get("/summary.html", summary);
+rootRouter.get("/manual.html", manual);
+
 
 export default rootRouter;   
