98b0441ab082c0dc54c7c0a7dbf3b05c33315969
jungjaechoi <www898975@gmail.com>
2021-11-10 17:47:42 +0900

    카카오 구글 로그인 구현

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,9 +1,31 @@
 import User from "../models/User.js";
 
 export const home = async (req, res) => {
+    
+    const {name, email} = req.query;
+    if (email!=null)
+    {
+        console.log('hi');
+        const useremailExists = await User.exists({email});
+        if(useremailExists){
+            console.log("이미 가입된 회원입니다.");
+        }
+        else{
+            try{
+                console.log('gg');
+                await User.create({
+                    name,
+                    email
+                });
+            }
+            catch(error){
+                console.log("db 저장과정에서 error 발생")
+            }
+        }
+    }
     return res.render("index.html");
 }; 
-
+ 
 export const down = async(req,res) => {
     return res.render("down.html");
 };
@@ -16,19 +38,3 @@ export const manual = async(req,res) => {
     return res.render("manual.html");
 };
 
-export const save_user_db = async(req,res) => {
-    console.log('hi');
-    const { name} = req.param;
-    const usernameExists = await User.exists({name});
-    if(usernameExists){
-        console.log("이미 가입된 회원입니다.");
-    }
-    try{
-        await User.create({
-            name
-        });
-    }
-    catch(error){
-        console.log("db 저장과정에서 error 발생")
-    }
-} 
--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true}
+    name: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true}
 })
 
 const User = mongoose.model('User', userSchema);
